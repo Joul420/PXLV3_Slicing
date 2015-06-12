eb2146a327e8c5792d55ca3e54d0bcf7aba05d24
Mickael Franc <franc.mickael@gmail.com>
2015-06-12 12:14:07 +0200

    Alternative menu$

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
   $("#showLeft").click(function(e){
     $(this).toggleClass('active');
     $('.cbp-spmenu-left').toggleClass('cbp-spmenu-open');
+    $('.col-image-cover').toggleClass('down');
   });
 });
