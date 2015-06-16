2d73387932a0b686f55ad18c8403f55d566bbea1
Mickael Franc <franc.mickael@gmail.com>
2015-06-16 23:04:26 +0200

    Design page article

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   $("#showLeft").click(function(e){
     $(this).toggleClass('active');
     $('.cbp-spmenu-left').toggleClass('cbp-spmenu-open');
-    $('.col-image-cover').toggleClass('down');
+    $('.col-fixed-300').toggleClass('down');
   });
 });
