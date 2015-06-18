a25f4cf32d9927cce0071549164d75c4926f8b3f
Mickael Franc <franc.mickael@gmail.com>
2015-06-18 14:47:53 +0200

    JQuery pour la taille des thumbs dans les propositions d'article

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
+  sizeContent();
   $("#showLeft").click(function(e){
     $(this).toggleClass('active');
     $('.cbp-spmenu-left').toggleClass('cbp-spmenu-open');
     $('.col-fixed-300').toggleClass('down');
   });
 });
+
+$(window).resize(sizeContent);
+function sizeContent() {
+  $(".tab-content").height($(".col-fixed-300").height()-103);
+  $(".thumbed-link").height(($(".tab-content").height()/5)-25);
+}
