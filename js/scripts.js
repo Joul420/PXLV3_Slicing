c958b98af3b5d246662db000d67aa6c2278b72f7
Mickael Franc <franc.mickael@gmail.com>
2015-06-24 12:29:54 +0200

    Fin base cat

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
   sizeContent();
-  $("#showLeft").click(function(e){
+  $('#showLeft').click(function(e){
     $(this).toggleClass('active');
     $('.cbp-spmenu-left').toggleClass('cbp-spmenu-open');
     $('.col-fixed-300').toggleClass('down');
   });
+  
+  $('.slider-content').append('<span class="freeze-button glyphicon glyphicon-pause"></span>')
+  $('.freeze-button').click(function(){$(this).parents('.card-box').toggleClass('active')})
 });
 
 $(window).resize(sizeContent);
 function sizeContent() {
-  $(".tab-content").height($(".col-fixed-300").height()-103);
-  $(".thumbed-link").height(($(".tab-content").height()/5)-25);
+  $('.tab-content').height($('.col-fixed-300').height()-103);
+  $('.thumbed-link').height(($('.tab-content').height()/5)-25);
 }
