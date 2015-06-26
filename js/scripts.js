c825b6cbc89f1ad087d7c47e2460effc6e73de00
Mickael Franc <franc.mickael@gmail.com>
2015-06-26 17:33:38 +0200

    Catégorie v2

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,18 @@ $(document).ready(function(){
   
   $('.slider-content').append('<span class="freeze-button glyphicon glyphicon-pause"></span>')
   $('.freeze-button').click(function(){$(this).parents('.card-box').toggleClass('active')})
+  
+  $('.caption').css('top',$('.thumbnail').outerHeight()-($('.caption h2').outerHeight()+$('.caption .accroche').outerHeight())-15);
+  $('.thumbnail').hover(function(){
+    $(this).children('.caption').css('top',15);
+  },function(){
+    $(this).children('.caption').css('top',$('.thumbnail').outerHeight()-($('.caption h2').outerHeight()+$('.caption .accroche').outerHeight())-15)
+  });
 });
 
 $(window).resize(sizeContent);
 function sizeContent() {
   $('.tab-content').height($('.col-fixed-300').height()-103);
   $('.thumbed-link').height(($('.tab-content').height()/5)-25);
+  $('.caption').css('top',$('.thumbnail').outerHeight()-($('.caption h2').outerHeight()+$('.caption .accroche').outerHeight())-15);
 }
